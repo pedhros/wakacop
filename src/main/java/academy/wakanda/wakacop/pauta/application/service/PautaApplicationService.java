5106a6a98f9c8b8dc88371eea22cb78b10ba6058
unknown <pedrooohsss@gmail.com>
2025-06-19 07:33:16 -0300

    implementa camada api e flow para service

---
+++ src/main/java/academy/wakanda/wakacop/pauta/application/service/PautaApplicationService.java
@@ -0,0 +1,20 @@
+package academy.wakanda.wakacop.pauta.application.service;
+
+import academy.wakanda.wakacop.pauta.application.api.NovaPautaRequest;
+import academy.wakanda.wakacop.pauta.application.api.PautaCadastraResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Log4j2
+public class PautaApplicationService implements PautaService{
+    @Override
+    public PautaCadastraResponse cadastraPauta(NovaPautaRequest novaPauta) {
+        log.info("[start] PautaApplicationService - cadastraPauta");
+        log.info("[NovaPauta] {}", novaPauta);
+        log.info("[finish] PautaApplicationService - cadastraPauta");
+        return null;
+    }
+}
